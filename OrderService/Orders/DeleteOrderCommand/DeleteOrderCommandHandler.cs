ba6f9bd3c2d93581ded2016e44c47e5bfaf50f6c
Lina <linachugainova0@gmail.com>
2025-07-18 12:48:27 +0500

    Add handler and endpoint for delete orders

---
+++ OrderService/Orders/DeleteOrderCommand/DeleteOrderCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace OrderService.Orders.DeleteOrderCommand
+{
+    public record DeleteOrderCommand(Guid Id) : ICommand<DeleteOrderResult>;
+    public record DeleteOrderResult(bool IsSuccess);
+
+    public class DeleteOrderCommandHandler(IDocumentSession session)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            var order = await session.LoadAsync<Order>(command.Id, cancellationToken);
+            if (order == null)
+            {
+                throw new Exception();
+            }
+
+            session.Delete(order);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteOrderResult(true);
+        }
+    }
+}
---
+++ OrderService/Orders/DeleteOrderCommand/DeleteOrderEndpoint.cs
@@ -0,0 +1,19 @@
+﻿
+namespace OrderService.Orders.DeleteOrderCommand
+{
+    public record DeleteOrderRequest(Guid Id);
+    public record DeleteOrderResponse(bool IsSuccess);
+    public class DeleteOrderEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/order/{id}", async (Guid id, ISender sender) =>
+            {
+                var command = new DeleteOrderCommand(id);
+                var result = await sender.Send(command);
+                var response = new DeleteOrderResponse(result.IsSuccess);
+                return Results.Ok(response);
+            });
+        }
+    }
+}
