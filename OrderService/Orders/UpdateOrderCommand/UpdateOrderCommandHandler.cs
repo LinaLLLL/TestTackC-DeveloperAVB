9f007f8c04177e886352f366b96d6089f516b3bb
Lina <linachugainova0@gmail.com>
2025-07-18 13:09:51 +0500

    Add handler and endpoint for update orders

---
+++ OrderService/Orders/UpdateOrderCommand/UpdateOrderCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace OrderService.Orders.UpdateOrderCommand
+{
+    public record UpdateOrderCommand(Guid Id, DateTime CreatedAt, List<OrderItem> Items, string Status)
+        : ICommand<UpdateOrderResult>;
+
+    public record UpdateOrderResult(bool IsSuccess);
+
+    public class UpdateOrderCommandHandler(IDocumentSession session)
+        : ICommandHandler<UpdateOrderCommand, UpdateOrderResult>
+    {
+        public async Task<UpdateOrderResult> Handle(UpdateOrderCommand command,
+            CancellationToken cancellationToken)
+        {
+            var order = await session.LoadAsync<Order>(command.Id, cancellationToken);
+
+            if (order is null)
+            {
+                throw new InvalidOperationException($"Order with ID {command.Id} not found."); 
+            }
+
+            command.Adapt(order);
+            session.Update(order);
+            await session.SaveChangesAsync(cancellationToken);
+            return new UpdateOrderResult(true);
+        }
+    }
+}
---
+++ OrderService/Orders/UpdateOrderCommand/UpdateOrderEndpoint.cs
@@ -0,0 +1,19 @@
+﻿namespace OrderService.Orders.UpdateOrderCommand
+{
+    public record UpdateOrderRequest(Guid Id, DateTime CreatedAt, List<OrderItem> Items, string Status);
+
+    public record UpdateOrderResponse(bool IsSuccess);
+    public class UpdateOrderEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateOrderCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateOrderResponse>();
+                return Results.Ok(response);
+            });
+        }
+    }
+}
