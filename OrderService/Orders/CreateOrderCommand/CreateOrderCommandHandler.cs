b02fc32ea188cef7721bcf9df759bc5d40d0910a
Lina <linachugainova0@gmail.com>
2025-07-18 12:20:46 +0500

    Add handler and endpoint for creating orders

---
+++ OrderService/Orders/CreateOrderCommand/CreateOrderCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace OrderService.Orders.CreateOrderCommand
+{
+
+    public record CreateOrderCommand(DateTime CreatedAt, List<OrderItem> Items, string Status) 
+        : ICommand<CreateOrderResult>;
+   
+    public record CreateOrderResult(Guid id);
+
+    public class CreateOrderCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public async Task<CreateOrderResult> Handle(CreateOrderCommand command, 
+            CancellationToken cancellationToken)
+        {
+            var order = new Order
+            {
+                CreatedAt = command.CreatedAt,
+                Items = command.Items,
+                Status = command.Status,
+            };
+            session.Store(order);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateOrderResult(order.Id);
+        }
+    }
+}
---
+++ OrderService/Orders/CreateOrderCommand/CreateOrderEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Carter;
+using Mapster;
+
+
+namespace OrderService.Orders.CreateOrderCommand
+{
+    public record CreateOrderRequest(DateTime CreatedAt, List<OrderItem> Items, string Status);
+    public record CreateOrderResponse(Guid id);
+    public class CreateOrderEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/order", async (CreateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateOrderCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateOrderResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("CreateOrder")
+            .WithSummary("Создание нового заказа")
+            .WithDescription("Создаёт заказ с переданными товарами и статусом через MediatR");
+        }
+    }
+}
