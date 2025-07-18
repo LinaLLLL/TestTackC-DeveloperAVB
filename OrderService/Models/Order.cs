c5cdf98bb17e0df8a5f9ff059812cf6dd8a89169
Lina <linachugainova0@gmail.com>
2025-07-18 17:38:20 +0500

    Update Order model

--- OrderService/Models/Order.cs
+++ OrderService/Models/Order.cs
@@ -3,8 +3,8 @@
     public class Order
     {
         public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public List<OrderItem> Items { get; set; }
-        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; } = default!;
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public string Status { get; set; } = string.Empty;
     }
 }
