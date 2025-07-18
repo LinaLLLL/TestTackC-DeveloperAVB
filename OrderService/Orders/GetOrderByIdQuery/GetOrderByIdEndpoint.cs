dcb0c991d4ee22a5829bc2ece49d9958218a6ab5
Lina <linachugainova0@gmail.com>
2025-07-18 17:36:53 +0500

    Add handler and endpoint for getting by Id orders

---
+++ OrderService/Orders/GetOrderByIdQuery/GetOrderByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace OrderService.Orders.GetOrderQuery
+{
+    public record GetOrderByIdRequest(Guid Id);
+    public record GetOrderByIdResponse(Order order);
+    public class GetOrderByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/{id}", async (Guid id, ISender sender) =>
+            {
+                var query = new GetOrderByIdQuery(id);
+                var result = await sender.Send(query);
+                if (result.Order == null)
+                {
+                    return Results.NotFound();
+                }
+                    
+                var response = result.Adapt<GetOrderByIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetOrderById")
+            .WithSummary("Получение заказа по ID")
+            .WithDescription("Выдает заказ по переданному идентификатору");
+        }
+    }
+}
---
+++ OrderService/Orders/GetOrderByIdQuery/GetOrderByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Marten.Linq.QueryHandlers;
+using OrderService.Orders.DeleteOrderCommand;
+
+namespace OrderService.Orders.GetOrderQuery
+{
+    public record GetOrderByIdQuery(Guid Id) : IQuery<GetOrderByIdResult>;
+    public record GetOrderByIdResult(Order? Order);
+    public class GetOrderByIdQueryHandler(IDocumentSession session)
+        : IQueryHandler<GetOrderByIdQuery, GetOrderByIdResult>
+    {
+        public async Task<GetOrderByIdResult> Handle(GetOrderByIdQuery query, CancellationToken cancellationToken)
+        {
+            var order = await session.LoadAsync<Order>(query.Id, cancellationToken);
+            if (order == null)
+            {
+                return new GetOrderByIdResult(null);
+            }
+            return new GetOrderByIdResult(order);
+        }
+    }
+}
