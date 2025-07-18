49934c12fd2c2d1b06c78496114622699b6785c1
Lina <linachugainova0@gmail.com>
2025-07-18 14:23:30 +0500

    Add handler and endpoint for getting orders

---
+++ OrderService/Orders/GetOrdersQuery/GetOrderQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderService.Orders.GetOrderQuery
+{
+    public record GetOrdersQuery(int? PageNumber = 1, int? PageSize = 5) : IQuery<GetOrdersResult>;
+    public record GetOrdersResult(IEnumerable<Order> orders);
+    public class GetOrderQueryHandler(IDocumentSession session) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+    {
+        public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await session.Query<Order>().ToPagedListAsync(query.PageNumber ?? 1,
+                query.PageSize ?? 5, cancellationToken);
+            return new GetOrdersResult(orders);
+        }
+    }
+}
---
+++ OrderService/Orders/GetOrdersQuery/GetOrdersEndpoint.cs
@@ -0,0 +1,22 @@
+﻿
+namespace OrderService.Orders.GetOrderQuery
+{
+    public record GetOrdersRequest(int? PageNumber = 1, int? PageSize = 5);
+    public record GetOrdersResponse(IEnumerable<Order> orders);
+    public class GetOrdersEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async ([AsParameters] GetOrdersRequest request, ISender sender) =>
+            {
+                GetOrdersQuery query = request.Adapt<GetOrdersQuery>();
+                GetOrdersResult result = await sender.Send(query);
+                GetOrdersResponse response = result.Adapt<GetOrdersResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetOrders")
+            .WithSummary("Получить список заказов")
+            .WithDescription("Возвращает постраничный список заказов.");
+        }
+    }
+}
