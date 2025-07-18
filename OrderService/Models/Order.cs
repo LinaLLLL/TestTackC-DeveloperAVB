dcf1ce57ffde5cdfc53b0aebebf7c0ca63bb0ff0
Lina <linachugainova0@gmail.com>
2025-07-18 11:48:15 +0500

    Add models (Order, OrderItem)

---
+++ OrderService/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderService.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<OrderItem> Items { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ OrderService/Models/OrderItem.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderService.Models
+{
+    public class OrderItem
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
